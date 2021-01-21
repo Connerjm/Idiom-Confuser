f89e86afee52c9f51accdf8be2e9605b65dfc928
Conner Martin <connerjm2@gmail.com>
2021-01-20 16:08:13 -0800

    Added a couple more bits to arrays and updated the readme.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,9 @@
 //Variables
 
-var firstHalfs = ["Even a broken clock", "A watched pot", "A bird in the hand", "A penny saved"];
-var secondHalfs = ["is right twice a day.", "never boils.", "is worth two in the bush.", "is a penny earned."];
-var fakeAuthors = ["Plato", "Socrates", "Aristotle", "Obama", "Some Old Dude", "That One Guy", "Galileo", "Issac Newton", "Einstein", "That Teacher that was always rude for no reason"];
-var after = [" or something.", " propbably.", " or whatever.", "???", " I think."];
+var firstHalfs = ["Even a broken clock", "A watched pot", "A bird in the hand", "A penny saved", "An apple a day"];
+var secondHalfs = ["is right twice a day.", "never boils.", "is worth two in the bush.", "is a penny earned.", "keeps the doctor away."];
+var fakeAuthors = ["Plato", "Socrates", "Aristotle", "Obama", "Some Old Dude", "That One Guy", "Galileo", "Issac Newton", "Einstein", "That Teacher that was always rude for no reason", "John 3:16", "A legitimate therapist"];
+var after = [" or something.", " probably.", " or whatever.", "???", " I think.", " I swear.", " according to FOX news.", " circa 2021"];
 
 //Functions
 
@@ -19,9 +19,9 @@ function newIdiom()//Grabs an entry from both of the arrays and creates a new me
 
 function newauthor()//Grabs an entry from both of the arrays to add a fake author of the quote.
 {
-    var x = Math.floor(Math.random() * fakeAuthors.length);
-    var y = Math.floor(Math.random() * after.length);
-    return fakeAuthors[x] + after[y];
+    var a = Math.floor(Math.random() * fakeAuthors.length);
+    var b = Math.floor(Math.random() * after.length);
+    return fakeAuthors[a] + after[b];
 }
 
 //Attaching listeners.
--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Idiom-Confuser
 
-I wanted to make a simple application that would randomly confuse two idioms into one sentence, so I did.
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Technologies](#technologies)
+- [Launch](#launch)
+- [Images](#images)
+- [To Do](#to-do)
+- [Issues](#issues)
+
+## Introduction
+
+I wanted to make a simple application that would randomly confuse two idioms into one sentence, so I did. It's a simple little application that pulls entries from a couple of arrays to create a new bit of nonsense, which is created at the push of the button. I will most likely keep adding to this to practice new concepts and such.
+
+## Technologies
+
+Project is created with:
+
+- HTML
+- CSS with Bootstrap 5.0.0
+- JavaScript with jQuery 3.5.1
+
+## Launch
+
+- [Repository](https://github.com/Connerjm/Idiom-Confuser)
+- [Deployment](https://connerjm.github.io/Idiom-Confuser/)
+
+## Images
+
+Nice and simple.
+
+![An image of a produced idiom.](Assets/Images/idiom.png)
+
+## To Do
+
+- [ ] Make the whole result shareable to other media.
+
+## Issues
+
+- None at present.
